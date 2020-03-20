2b3a01ffb1ef68cdd1d01924bb852fc9138e5c46
theskymaks <theskymaks@gmail.com>
2020-03-20 09:47:11 +0200

    Added 4,5,6 labs

--- 5.3. e-olymp 1534. Делимость/5.3. e-olymp 1534. Делимость.cs
+++ 5.3. e-olymp 1534. Делимость/5.3. e-olymp 1534. Делимость.cs
@@ -1,12 +1,81 @@
-﻿using System;
+﻿/* Делимость
+Имеется набор целых чисел a1, a2, ..., an. Найти количество чисел от l до r включительно, 
+которые делятся хотя бы на одно число из этого набора.
+Входные данные:
+Состоит из нескольких тестов. Первая строка каждого теста содержит два целых числа l (1 ≤ l ≤ 109) и r (1 ≤ r ≤ 109). 
+Следующая строка содержит количество элементов n (1 ≤ n ≤ 18) в наборе и сам набор этих чисел. 
+Каждое число в наборе принимает значение от 1 до 109.
+Выходные данные:
+Для каждого теста в отдельной строке вывести количество чисел от l до r включительно, 
+которые делятся хотя бы на одно из чисел из набора a1, a2, ..., an.
+ */
+using System;
 
-namespace _5._3._e_olymp_1534._Делимость
+namespace _5._3._e_olymp_1534._Делимость //by TheSkyMaks
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                string[] splitString = Console.ReadLine().Split(' ');
+                if (!int.TryParse(splitString[0], out int leftSide))
+                {
+                    break;
+                }
+                int.TryParse(splitString[1], out int rightSide);
+                string[] sDivisors = Console.ReadLine().Split(' ');
+                int[] divisors = new int[20];
+                int amountOfDivisor = sDivisors.Length;
+                for (int i = 0; i < amountOfDivisor; ++i)
+                {//O(amountOfDivisor)
+                    int.TryParse(sDivisors[i], out divisors[i]);
+                }
+                double result = Algorithm(rightSide, divisors, amountOfDivisor) - Algorithm(leftSide - 1, divisors, amountOfDivisor);
+                Console.WriteLine(result);
+            }
         }
-    }
+        private static long TheGreatestCommonDivisor(long a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return TheGreatestCommonDivisor(b, Convert.ToInt32(a % b));
+        }
+        private static long Algorithm(int chyslo, int[] divide, int amountOfDivisor)
+        {
+            long GCD, result = 0;
+            for (int i = 1; i < Math.Pow(2, amountOfDivisor); ++i) //1 << amount_of_divisor
+            {//O(2^amountOfDivisor)
+                long theGreatestCommonFactor = 1;
+                int bits = 0;
+                for (int j = 0; j < amountOfDivisor; ++j)
+                {//O(amountOfDivisor)
+                    if ((i & (1 << j)) != 0)
+                    {// checks if the j - th bit of counter is set. In more detail, 1 << j uses shifting of 1 to generate a bit mask 
+                     //in which only the j-th bit is set.The & operator then masks out the j-bit of counter; 
+                     //if the result is not zero (which means that the j-th bit of counter was set), the condition is satisfied.
+                        bits++;
+                        GCD = TheGreatestCommonDivisor(theGreatestCommonFactor, divide[j]);
+                        theGreatestCommonFactor = theGreatestCommonFactor / GCD * divide[j];
+                        if (theGreatestCommonFactor > chyslo)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (bits % 2 != 0)
+                {
+                    result += chyslo / theGreatestCommonFactor;
+                }
+                else
+                {
+                    result -= chyslo / theGreatestCommonFactor;
+                }
+            }
+            return result;
+        }
+    }//O(amountOfDivisor^2 x 2^amountOfDivisor)
 }
---
+++ 6.3. e-olymp 1505. Пересечение отрезков - 2/6.3. e-olymp 1505. Пересечение отр.cs
@@ -0,0 +1,59 @@
+﻿/* Пересечение отрезков - 2
+На декартовой плоскости задано n отрезков координатами своих концов. Определить, пересекаются ли они. Множество отрезков пересекается, 
+если среди них существует хотя бы два, которые имеют как минимум одну общую точку.
+Входные данные
+Каждая строка содержит целочисленные координаты концов отрезка (x1[i], y1[i]) - (x2[i], y2[i]). 
+Известно, что n ≤ 6*105и -2000 ≤ x1[i], y1[i], x2[i], y2[i] ≤ 2000.
+Выходные данные
+Вывести "intersect" если отрезки пересекаются и "NOT intersect" иначе.
+ */
+using System;
+
+namespace _6._3._e_olymp_1505._Пересечение_отрезков___2 //by TheSkyMaks
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] x1 = new int[20];
+            int[] y1 = new int[20];
+            int[] x2 = new int[20];
+            int[] y2 = new int[20];
+
+            for (int i = 0; i < 20; i++)
+            {//O(20)
+                string[] splitString = Console.ReadLine().Split(' ');//отрезок                
+                if (!int.TryParse(splitString[0], out x1[i]))
+                {
+                    break;
+                }
+                int.TryParse(splitString[1], out y1[i]);
+                int.TryParse(splitString[2], out x2[i]);
+                int.TryParse(splitString[3], out y2[i]);
+                for (int j = 0; j < i; j++)
+                {//0(i)
+
+                    int A1 = y2[i] - y1[i];
+                    int B1 = x1[i] - x2[i];
+                    int C1 = (-A1 * x1[i]) - (B1 * y1[i]);
+
+                    int A2 = y2[j] - y1[j];
+                    int B2 = x1[j] - x2[j];
+                    int C2 = (-A2 * x1[j]) - (B2 * y1[j]);
+
+                    int f1 = (A1 * x1[j]) + (B1 * y1[j]) + C1;
+                    int f2 = (A1 * x2[j]) + (B1 * y2[j]) + C1;
+                    int f3 = (A2 * x1[i]) + (B2 * y1[i]) + C2;
+                    int f4 = (A2 * x2[i]) + (B2 * y2[i]) + C2;
+
+                    if (f1 * f2 < 0 && f3 * f4 < 0) // строгое пересечение
+                    {
+                        Console.WriteLine("intersect");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("NOT intersect");
+        }
+    }//0(20 x i)
+}
